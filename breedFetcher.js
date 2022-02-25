a57a856ef920ef50ca5d963389fed124ee8cb85d
Ziyu Huang <aprilhuang825@gmail.com>
2022-02-25 01:15:42 +0000

    print out breed description by name

---
+++ breedFetcher.js
@@ -0,0 +1,13 @@
+const request = require('request');
+const breedName = process.argv[2];
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+  if (error){
+    console.log('error:', error); 
+  }
+  const data = JSON.parse(body);
+  if (data.length === 0) {
+    console.log("The requested breed is not found");
+  } else {
+    console.log(data[0].description);
+  }
+});
